263c4b259dfdd11a0702d21d5ae0c1a118418b57
Alex Krantz <alex@krantz.dev>
2022-01-14 17:34:53 -0800

    Add graceful shutdown

--- lights-web/main.go
+++ lights-web/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"context"
-	"io"
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"capnproto.org/go/capnp/v3/rpc"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"go.uber.org/zap"
 
 	"github.com/akrantz01/lights/lights-web/lights"
 	"github.com/akrantz01/lights/lights-web/logging"
@@ -22,31 +26,68 @@ func main() {
 	}
 	defer logger.Sync()
 
-	conn, err := net.Dial("tcp", "192.168.1.6:30000")
-	if err != nil {
-		log.Fatalf("failed to dial: %v\n", err)
-	}
-
-	if err := client(context.Background(), conn); err != nil {
-		log.Fatalf("failed to execute: %v\n", err)
-	}
-
 	r := chi.NewRouter()
+
+	// Register middleware
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(logging.Request(logger))
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Heartbeat("/ping"))
-	if err := http.ListenAndServe(":3000", r); err != nil {
-		log.Fatalf("failed to bind HTTP server: %v\n", err)
+
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
+	server := &http.Server{
+		Addr:    "127.0.0.1:3000",
+		Handler: r,
 	}
+
+	// Listen for syscall signals for process to interrupt/quit
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-sig
+
+		// Create a 30s shutdown timeout
+		shutdownCtx, _ := context.WithTimeout(serverCtx, 30*time.Second)
+
+		go func() {
+			<-shutdownCtx.Done()
+			if shutdownCtx.Err() == context.DeadlineExceeded {
+				logger.Fatal("graceful shutdown timed out... forcing exit")
+			}
+		}()
+
+		// Trigger graceful shutdown
+		err := server.Shutdown(shutdownCtx)
+		if err != nil {
+			logger.Fatal("failed to shutdown server", zap.Error(err))
+		}
+		serverStopCtx()
+	}()
+
+	// Start the server
+	logger.Info("listening and ready to handle requests", zap.String("address", "127.0.0.1:3000"))
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Fatal("an error occurred while running", zap.Error(err))
+	}
+
+	// Wait for server context to be stopped
+	<-serverCtx.Done()
+
+	logger.Info("shutdown complete. goodbye!")
 }
 
-func client(ctx context.Context, rwc io.ReadWriteCloser) error {
-	conn := rpc.NewConn(rpc.NewStreamTransport(rwc), nil)
-	defer conn.Close()
+func client() error {
+	ctx := context.Background()
+	conn, err := net.Dial("tcp", "192.168.1.6:30000")
+	if err != nil {
+		log.Fatalf("failed to dial: %v\n", err)
+	}
 
-	lc := lights.LightController{Client: conn.Bootstrap(ctx)}
+	rpcConn := rpc.NewConn(rpc.NewStreamTransport(conn), nil)
+	defer rpcConn.Close()
+
+	lc := lights.LightController{Client: rpcConn.Bootstrap(ctx)}
 
 	lc.Brightness(ctx, func(params lights.LightController_brightness_Params) error {
 		params.SetLevel(60)
@@ -59,9 +100,9 @@ func client(ctx context.Context, rwc io.ReadWriteCloser) error {
 			return err
 		}
 
-		c.SetR(96)
-		c.SetG(0)
-		c.SetB(255)
+		c.SetR(196)
+		c.SetG(128)
+		c.SetB(64)
 
 		return nil
 	})
