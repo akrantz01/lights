dfa94004a211c8a592cb745fb8a78a215cb65e93
Alex Krantz <alex@krantz.dev>
2022-01-15 16:32:50 -0800

    Make upgrader local

--- lights-web/ws/handler.go
+++ lights-web/ws/handler.go
@@ -8,16 +8,17 @@ import (
 	"go.uber.org/zap"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
 // Handler initiates the websocket connection and starts the client
 func Handler(hub *Hub, stripLength int) func(w http.ResponseWriter, r *http.Request) {
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:    1024,
+		WriteBufferSize:   1024,
+		EnableCompression: true,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		conn, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
