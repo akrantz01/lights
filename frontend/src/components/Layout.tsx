4c13e040aba6905202a27d70ead80ffc34b33518
Alex Krantz <alex@krantz.dev>
2022-01-31 20:07:08 -0800

    Switch to grayscale theme

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -14,7 +14,7 @@ const Layout = ({ children }: Props): JSX.Element => {
 
         <main className="-mt-32">
           <div className="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
-            <div className="bg-white rounded-lg shadow px-5 py-6 sm:px-6">{children}</div>
+            <div className="bg-gray-200 rounded-lg shadow px-5 py-6 sm:px-6">{children}</div>
           </div>
         </main>
       </div>
