dde1f99dd6f5bdc6a5333cd5211ac689bd4aebfc
Alex Krantz <alex@krantz.dev>
2022-02-03 18:17:11 -0800

    Remove link to detail page for animations

--- frontend/src/animations/Animations.tsx
+++ frontend/src/animations/Animations.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FilmIcon } from '@heroicons/react/outline';
-import { ChevronRightIcon } from '@heroicons/react/solid';
-import { Link, RouteComponentProps } from '@reach/router';
+import { RouteComponentProps } from '@reach/router';
 
 import Button from '../components/Button';
 import ListView from '../components/ListView';
@@ -33,21 +32,16 @@ const Animations: React.FC<RouteComponentProps> = () => {
       >
         {(item) => (
           <li key={item}>
-            <Link to={`/animations/${item}`} className="block hover:bg-gray-50">
-              <div className="px-4 py-4 flex items-center sm:px-6">
-                <div className="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
-                  <p className="text-sm font-medium text-indigo-600 truncate">{item}</p>
-                  <div className="mt-4 flex-shrink-0 sm:mt-0 sm:ml-5">
-                    <Button onClick={apply(item)} style="secondary">
-                      Start
-                    </Button>
-                  </div>
-                </div>
-                <div className="ml-5 flex-shrink-0">
-                  <ChevronRightIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+            <div className="px-4 py-4 flex items-center sm:px-6">
+              <div className="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
+                <p className="text-sm font-medium text-indigo-600 truncate">{item}</p>
+                <div className="mt-4 flex-shrink-0 sm:mt-0 sm:ml-5">
+                  <Button onClick={apply(item)} style="secondary">
+                    Start
+                  </Button>
                 </div>
               </div>
-            </Link>
+            </div>
           </li>
         )}
       </ListView>
