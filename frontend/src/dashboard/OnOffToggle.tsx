8070744ef2c7af3043efccd82e0b44d018e42a87
Alex Krantz <alex@krantz.dev>
2022-02-06 17:26:13 -0800

    Use toggle component for on/off

--- frontend/src/dashboard/OnOffToggle.tsx
+++ frontend/src/dashboard/OnOffToggle.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Switch } from '@headlessui/react';
-import { CheckIcon, XIcon } from '@heroicons/react/outline';
-import classNames from 'classnames';
 
+import { Toggle } from '../components/form';
 import { turnOff, turnOn, useDispatch, useSelector } from '../store';
 
 const OnOffToggle = (): JSX.Element => {
@@ -15,41 +14,7 @@ const OnOffToggle = (): JSX.Element => {
     <Switch.Group>
       <div className="flex items-center justify-between">
         <Switch.Label className="text-2xl">Lights</Switch.Label>
-        <Switch
-          checked={on}
-          onChange={onChange}
-          className={classNames(
-            on ? 'bg-indigo-600' : 'bg-gray-400',
-            'relative inline-flex flex-shrink h-9 w-20 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
-          )}
-        >
-          <span className="sr-only">Change state</span>
-          <span
-            className={classNames(
-              on ? 'translate-x-11' : 'translate-x-0',
-              'pointer-events-none relative inline-block h-8 w-8 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200',
-            )}
-          >
-            <span
-              className={classNames(
-                on ? 'opacity-0 ease-out duration-100' : 'opacity-100 ease-in duration-200',
-                'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity',
-              )}
-              aria-hidden="true"
-            >
-              <XIcon className="h-5 w-5 text-gray-400" />
-            </span>
-            <span
-              className={classNames(
-                on ? 'opacity-100 ease-in duration-200' : 'opacity-0 ease-out duration-100',
-                'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity',
-              )}
-              aria-hidden="true"
-            >
-              <CheckIcon className="h-5 w-5 text-gray-400" />
-            </span>
-          </span>
-        </Switch>
+        <Toggle enabled={on} onChange={onChange} large />
       </div>
     </Switch.Group>
   );
