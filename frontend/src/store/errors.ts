33a1dae56a7f5725ca034a063c8194efde98584c
Alex Krantz <alex@krantz.dev>
2022-02-20 19:21:58 -0800

    Don't notify of error on 404

--- frontend/src/store/errors.ts
+++ frontend/src/store/errors.ts
@@ -4,7 +4,7 @@ import { toast } from 'react-hot-toast';
 const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
 
 const errorLogger: Middleware = () => (next) => (action) => {
-  if (isRejectedWithValue(action)) {
+  if (isRejectedWithValue(action) && action.payload.status !== 404) {
     toast.error(capitalize(action.payload.data.reason));
   }
 
