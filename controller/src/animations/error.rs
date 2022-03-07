0726e8a71ba043c334eac69c1e91e718fa0cad1b
Alex Krantz <alex@krantz.dev>
2022-03-06 16:00:08 -0800

    Display inner errors

--- controller/src/animations/error.rs
+++ controller/src/animations/error.rs
@@ -4,9 +4,9 @@ use wasmer::{CompileError, DeserializeError, ExportError, InstantiationError, Se
 
 #[derive(Debug, Error)]
 pub enum BuildError {
-    #[error("failed to compile animation")]
+    #[error("failed to compile animation: {0}")]
     Compilation(#[from] CompileError),
-    #[error("failed to load animation")]
+    #[error("failed to load animation: {0}")]
     Instantiation(#[from] InstantiationError),
     #[error("invalid signature for animate function")]
     InvalidSignature,
@@ -27,11 +27,11 @@ impl From<ExportError> for BuildError {
 pub enum LoadError {
     #[error("couldn't find the animation")]
     NotFound,
-    #[error("failed to read file")]
+    #[error("failed to read file: {0}")]
     IO(#[source] io::Error),
-    #[error("failed to parse animation")]
+    #[error("failed to parse animation: {0}")]
     Deserialization(#[from] DeserializeError),
-    #[error("failed to load animation")]
+    #[error("failed to load animation: {0}")]
     Instantiation(#[from] InstantiationError),
 }
 
@@ -46,16 +46,16 @@ impl From<io::Error> for LoadError {
 
 #[derive(Debug, Error)]
 pub enum RegistrationError {
-    #[error("failed to build animation")]
+    #[error("failed to build animation: {0}")]
     BuildError(#[from] BuildError),
-    #[error("failed to save animation")]
+    #[error("failed to save animation: {0}")]
     SaveError(#[from] SaveError),
 }
 
 #[derive(Debug, Error)]
 pub enum SaveError {
-    #[error("failed to write to file")]
+    #[error("failed to write to file: {0}")]
     IO(#[from] io::Error),
-    #[error("failed to serialize animation")]
+    #[error("failed to serialize animation: {0}")]
     Serialization(#[from] SerializeError),
 }
